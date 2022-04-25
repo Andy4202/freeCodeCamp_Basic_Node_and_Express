393993587b2551945cf81d1fef50bef05dfb893f
Andy Allan <Andy4202@gmail.com>
2022-04-25 20:19:38 +0100

    myApps.js updated to use express.static()

--- myApp.js
+++ myApp.js
@@ -3,6 +3,8 @@ var app = express();
 
 console.log('Hello World')
 
+app.use('/public', express.static(__dirname + '/public'));
+
 app.get("/", function(req, res){
     const absolutePath = __dirname + '/views/index.html';
     res.sendFile(absolutePath);
