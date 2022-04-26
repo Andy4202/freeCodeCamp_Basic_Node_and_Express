2f56aec5e8c4ada72627aa432ed4f251a57b2a36
Andy Allan <Andy4202@gmail.com>
2022-04-26 19:51:01 +0100

    bodyparser added to myApp.js

--- myApp.js
+++ myApp.js
@@ -1,7 +1,12 @@
-require('dotenv').config()
+require('dotenv').config();
 
+var bodyParser = require('body-parser');
 var express = require('express');
 var app = express();
+
+
+app.use('/', bodyParser.urlencoded({extended: false}))
+
 
 app.use('/public', express.static(__dirname + '/public'));
 
